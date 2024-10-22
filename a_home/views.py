af516fa6137461cee74e8c7ff429b5b502bfeda5
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-10-22 08:17:59 +0200

    Add SurveyAnswer model and survey_results_view

--- a_home/views.py
+++ a_home/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 from a_bot.views import get_text_message_input,send_message
 from django.contrib.auth.decorators import login_required
+from .models import SurveyAnswer
 
 
 # Create your views here.
@@ -15,6 +16,15 @@ def home_view(request):
     all_users = Individual.objects.all()
     all_groups =Group.objects.all()
     return render(request,'home.html',{'users': all_users,'groups': all_groups})
+
+
+
+@login_required
+def survey_results_view(request):
+    if request.user.is_staff:  # Assuming managing people have 'staff' status
+        survey_answers = SurveyAnswer.objects.all()  # Fetch all answers
+        return render(request, 'survey_results.html', {'survey_answers': survey_answers})
+    return redirect('home')  
 
 
 # Create new individual
