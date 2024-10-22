f5cdc0448eb491194ac93cfda0ce7cb4c30f4c68
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-10-22 09:36:45 +0200

    Refactor admin.py and add model admins
    - Register JobSatisfactionQuestion, LikertScaleAnswer, and DemographicData models in admin.py
    - Define custom admin classes with list display, list filter, and search fields for each model
    - Improve organization and readability of admin.py file

--- a_home/admin.py
+++ a_home/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(JobSatisfactionQuestion)
+class JobSatisfactionQuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'category', 'required']
+    list_filter = ['category', 'required']
+    search_fields = ['question_text']
+    
+@admin.register(LikertScaleAnswer)
+class LikertScaleAnswerAdmin(admin.ModelAdmin):
+    list_display = ['user_id', 'question', 'response', 'response_date']
+    list_filter = ['response_date']
+    search_fields = ['question__question_text']
+
+@admin.register(DemographicData)
+class DemographicAdmin(admin.ModelAdmin):
+    list_display = ['gender', 'age_group', 'work_experience', 'highest_qualification', 'designation', 'department', 'contract_type']
+    list_filter =['gender','highest_qualification']
+    
+
