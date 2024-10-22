a1ad039dddb0d691a4e1fc0d1f05be5ee7eab3cc
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-10-22 14:01:13 +0200

    Refactor URL path for demographic data view

--- a_home/urls.py
+++ a_home/urls.py
@@ -3,7 +3,7 @@ from a_home.views import *
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('start/', demographic_data_view, name='start'),
+    path('smedco/', demographic_data_view, name='smedco'),
     path('thank-you/', thank_you, name='thank_you'),
     path('job-satisfaction/', job_satisfaction_view, name='job_satisfaction'),
     path('aggregated-feedback/', aggregated_feedback_view, name='aggregated_feedback'),
