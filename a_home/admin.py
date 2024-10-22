ec3ed07c33fd70428bd23434593ef7238bafdfa7
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-10-22 10:24:39 +0200

    Refactor admin.py and form_styles.css, and update job_satisfaction.html

--- a_home/admin.py
+++ a_home/admin.py
@@ -4,19 +4,22 @@ from .models import *
 # Register your models here.
 @admin.register(JobSatisfactionQuestion)
 class JobSatisfactionQuestionAdmin(admin.ModelAdmin):
-    list_display = ['question_text', 'category', 'required']
+    list_display = ['id','question_text', 'category', 'required']
+    list_display_links = ['id','question_text']
     list_filter = ['category', 'required']
     search_fields = ['question_text']
     
 @admin.register(LikertScaleAnswer)
 class LikertScaleAnswerAdmin(admin.ModelAdmin):
-    list_display = ['user_id', 'question', 'response', 'response_date']
+    list_display = ['id', 'question', 'response', 'response_date']
+    list_display_links = ['id', 'question']
     list_filter = ['response_date']
     search_fields = ['question__question_text']
 
 @admin.register(DemographicData)
 class DemographicAdmin(admin.ModelAdmin):
-    list_display = ['gender', 'age_group', 'work_experience', 'highest_qualification', 'designation', 'department', 'contract_type']
+    list_display = ['id','gender', 'age_group', 'work_experience', 'highest_qualification', 'designation', 'department', 'contract_type']
+    list_display_links = ['id','gender']
     list_filter =['gender','highest_qualification']
     
 
