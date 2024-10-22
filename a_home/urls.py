fa7703d77edf77d3b1e56379e0006b6f85671283
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-10-22 11:10:47 +0200

    Refactor URLs to add new path for aggregated feedback view

--- a_home/urls.py
+++ a_home/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('start/', demographic_data_view, name='start'),
     path('thank-you/', thank_you, name='thank_you'),
     path('job-satisfaction/', job_satisfaction_view, name='job_satisfaction'),
+    path('aggregated-feedback/', aggregated_feedback_view, name='aggregated_feedback'),
     # path('members/',test,name='members')
     
 ]
--- a_home/views.py
+++ a_home/views.py
@@ -8,12 +8,76 @@ from django.views.decorators.csrf import csrf_exempt
 from a_bot.views import get_text_message_input,send_message
 from django.contrib.auth.decorators import login_required
 from .models import *
+from django.db.models import Count, Sum, Avg
+
+
 
 
 # Create your views here.
 def home_view(request):
-   
     return render(request,'home.html')
+
+@login_required
+def staff_dashboard_view(request):
+    total_participants = LikertScaleAnswer.objects.values('user_id').distinct().count()
+    
+    most_proper_department = DemographicData.objects.values('department').annotate(
+        avg_feedback=Avg('likertscaleanswer__response')
+    ).order_by('-avg_feedback').first()
+
+    average_qualification = DemographicData.objects.aggregate(
+        Avg('highest_qualification')
+    )
+
+    context = {
+        'total_participants': total_participants,
+        'most_proper_department': most_proper_department,
+        'average_qualification': average_qualification,
+    }
+
+    return render(request, 'staff/staff_dashboard.html', context)
+
+@login_required
+def aggregated_feedback_view(request):
+    total_responses = LikertScaleAnswer.objects.count()
+
+    question_summary = {}
+
+    questions = JobSatisfactionQuestion.objects.all()
+
+    for question in questions:
+        responses = LikertScaleAnswer.objects.filter(question=question)
+        count_responses = responses.count()
+        
+        if count_responses > 0:
+            avg_response = responses.aggregate(Avg('response'))['response__avg']
+            percentage_positive = responses.filter(response__gte=4).count() / count_responses * 100
+            
+            question_summary[question] = {
+                'average': avg_response,
+                'percentage_positive': percentage_positive,
+                'count': count_responses
+            }
+
+    departments_feedback = DemographicData.objects.values('department').annotate(
+        avg_feedback=Avg('likertscaleanswer__response')
+    ).order_by('avg_feedback')
+
+    worst_department = departments_feedback.first() if departments_feedback else None
+
+    salary_complaints = LikertScaleAnswer.objects.filter(
+        question__category='pay', 
+        response__lte=3
+    ).count()
+
+    context = {
+        'total_responses': total_responses,
+        'question_summary': question_summary,
+        'worst_department': worst_department,
+        'salary_complaints': salary_complaints
+    }
+
+    return render(request, 'aggregated_feedback.html', context)
 
 @login_required
 def demographic_data_view(request):
